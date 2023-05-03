1a834a22d4460b0b3eb39c1970f679fab9d1fd20
kauankarvalho <kauan.karvalho@outlook.com>
2023-05-02 22:20:32 -0300

    feat: add style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
-  theme: {},
+  theme: {
+    fontFamily: {
+      mulish: ["Mulish", "sans-serif"],
+      open_sans: ["Open Sans", "sans-serif"],
+      dm_sans: ["DM Sans", "sans-serif"],
+    },
+  },
   plugins: [],
 }
